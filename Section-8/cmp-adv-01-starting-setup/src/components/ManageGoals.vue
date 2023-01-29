d77ee66db0c937a26b49009c44c85a15b2dbd43d
khoahoang555 <khoahoangyahoo@gmail.com>
2023-01-29 22:47:25 +0700

    Section 8: Diving Deeper Into Components

---
+++ Section-8/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -0,0 +1,38 @@
+<template>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
+  <teleport to="body">
+    <error-aler v-if="inputIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please enter at least a few characters...</p>
+      <button @click="confirmError">Okay</button>
+    </error-aler>
+  </teleport>
+</template>
+
+<script>
+import ErrorAler from "@/components/ErrorAler.vue";
+
+export default {
+  components: {
+    ErrorAler
+  },
+  data() {
+    return {
+      inputIsInvalid: false
+    }
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === '') {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    }
+  }
+}
+</script>
