64587af1c7c6f6635e263621a9bad1631db0aa93
khoahoang555 <khoahoangyahoo@gmail.com>
2023-06-26 08:25:49 +0700

    Update source code

---
+++ New-Tutorial/vue-project/src/components/Exercise1-02.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ isUppercase ? title.toUpperCase() : title }}</h1>
+  </div>
+</template>
+<!--<script>-->
+<!--Option API-->
+<!--export default {-->
+<!--  data() {-->
+<!--    return {-->
+<!--      title: 'My first component!',-->
+<!--      isUppercase: false-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+<script setup>
+// Composition API
+const title = 'My first component!';
+const isUppercase = false;
+</script>
+<style scoped>
+</style>
---
+++ New-Tutorial/vue-project/src/components/Exercise1-03.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 v-text="text">Loading...</h1>
+  </div>
+</template>
+
+<script setup>
+const text = 'Directive text';
+</script>
+
+<style></style>
