0e312fbe4f9ee5e2a2b7d661a485b2cbdd6a5464
khoa pham <khoa.pham@tpssoft.com>
2023-07-10 18:36:01 +0700

    Update source code Section-2

---
+++ New-Tutorial/Section-2/src/components/Exercise2-01.vue
@@ -0,0 +1,21 @@
+<template>
+  <input placeholder="First name" v-model="firstName" />
+  <input placeholder="Last name" v-model="lastName" />
+  <h3>{{ fullName }}</h3>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: ''
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`
+    }
+  }
+}
+</script>
---
+++ New-Tutorial/Section-2/src/components/Exercise2-02.vue
@@ -0,0 +1,28 @@
+<template>
+  <input type="number" v-model="incrementOne" />
+  <h3>Get input: {{ incrementOne }}</h3>
+  <h5>Set division: {{ divideByTwo }}</h5>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 1,
+      divideByTwo: 0
+    }
+  },
+  computed: {
+    incrementOne: {
+      get() {
+        return this.count + 1;
+      },
+      set(val) {
+        this.count = val - 1;
+        this.divideByTwo = val / 2;
+        // console.log(this.count);
+      }
+    }
+  }
+}
+</script>
