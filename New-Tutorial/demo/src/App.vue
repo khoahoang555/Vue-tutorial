ca3e659adf5e9577fd93ce7e4443506da6ede3dc
khoa pham <khoa.pham@tpssoft.com>
2023-07-28 18:27:19 +0700

    Update source code

---
+++ New-Tutorial/demo/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <select v-model="nameComponent">
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+
+  <keep-alive>
+    <component :is="nameComponent"></component>
+  </keep-alive>
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+export default {
+  name: 'App',
+  components: {
+    Home,
+    About
+  },
+  data() {
+    return {
+      nameComponent: 'Home'
+    }
+  },
+  watch: {
+    nameComponent(newVal, oldVal) {
+      // console.log(newVal);
+    }
+  }
+}
+</script>
--- complete-vue-js-developer-zero-to-mastery-vuex/section-7-1/src/App.vue
+++ complete-vue-js-developer-zero-to-mastery-vuex/section-7-1/src/App.vue
@@ -4,9 +4,42 @@
 <!--    <h2 v-if="flag">Hello World!</h2>-->
 <!--    <h2 v-else="!flag">Another hello!</h2>-->
 <!--  </transition>-->
-  <transition name="zoom" type="animation" appear>
-    <h2 v-if="flag">Hello</h2>
-  </transition>
+<!--  <transition name="zoom" type="animation" appear>-->
+<!--    <h2 v-if="flag">Hello</h2>-->
+<!--  </transition>-->
+<!--  <transition-->
+<!--    @before-enter="beforeEnter"-->
+<!--    @enter="enter"-->
+<!--    @after-enter="afterEnter"-->
+<!--    @before-leave="beforeLeave"-->
+<!--    @leave="leave"-->
+<!--    @after-leave="afterLeave"-->
+<!--    :css="false"-->
+<!--  >-->
+<!--    <h2 v-if="flag">Hey</h2>-->
+<!--  </transition>-->
+
+  <button @click="addItem">Add</button>
+
+  <ul>
+    <transition-group name="fade"
+      enter-active-class="animate__animated animate__flipInX"
+      leave-active-class="animate__animated animate__flipOutX"
+    >
+      <li v-for="(number, index) in numbers" :key="number"
+        @click="removeItem(index)"
+      >
+        {{ number }}
+      </li>
+    </transition-group>
+<!--    <transition-group name="fade">-->
+<!--      <li v-for="(number, index) in numbers" :key="number"-->
+<!--        @click="removeItem(index)"-->
+<!--      >-->
+<!--        {{ number }}-->
+<!--      </li>-->
+<!--    </transition-group>-->
+  </ul>
 </template>
 
 <script>
@@ -14,13 +47,70 @@ export default {
   name: 'App',
   data() {
     return {
-      flag: true
+      flag: true,
+      numbers: [1, 2, 3, 4, 5]
+    }
+  },
+  methods: {
+    addItem() {
+      const num = Math.floor(Math.random() * 100 + 1);
+      const index = Math.floor(Math.random() * this.numbers.length)
+      this.numbers.splice(index, 0, num);
+    },
+    removeItem(index) {
+      this.numbers.splice(index, 1);
+    },
+    beforeEnter(el) {
+      console.log('before-enter event fired', el);
+    },
+    enter(el, done) {
+      console.log('enter event fired', el);
+
+      const animation = el.animate([{ transform: "scale3d(0,0,0)" }, {}], {
+        duration: 1000,
+      });
+
+      animation.onfinish = () => {
+        done();
+      }
+    },
+    afterEnter(el) {
+      console.log('after-enter event fired', el);
+    },
+    beforeLeave(el) {
+      console.log('before-leave event fired', el);
+    },
+    leave(el, done) {
+      console.log('leave event fired', el);
+      const animation = el.animate([{}, { transform: "scale3d(0,0,0)" }], {
+        duration: 1000,
+      });
+
+      animation.onfinish = () => {
+        done();
+      }
+    },
+    afterLeave(el) {
+      console.log('after leave event fired', el);
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+.animate__flipOutX {
+  position: absolute;
+}
+
+.animate_animated {
+  animation-duration: 1.5s;
+}
+
+li {
+  font-size: 22px;
+  cursor: pointer;
+}
+
 h2 {
   width: 400px;
   padding: 20px;
@@ -38,6 +128,10 @@ h2 {
 .fade-leave-to {
   transition: all 0.25s linear;
   opacity: 0;
+}
+
+.fade-move {
+  transition: all 1s linear;
 }
 
 .zoom-enter-active {
